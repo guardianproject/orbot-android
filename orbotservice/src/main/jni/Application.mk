b78e25023979c5f15f831a932798fcb9516eb341
a <dsnake@protonmail.com>
2025-06-23 03:48:51 -0400

    link native methods to OrbotVpnManager.java, where they are used

--- orbotservice/src/main/jni/Application.mk
+++ orbotservice/src/main/jni/Application.mk
@@ -1,6 +1,6 @@
 APP_OPTIM := debug
 APP_PLATFORM := android-24
 APP_ABI := x86 x86_64 armeabi-v7a arm64-v8a
-APP_CFLAGS := -O3 -DPKGNAME=org/torproject/android/service
+APP_CFLAGS := -O3 -DCLSNAME=OrbotVpnManager -DPKGNAME=org/torproject/android/service/vpn
 APP_CPPFLAGS := -O3 -std=c++11
 NDK_TOOLCHAIN_VERSION := clang
