bace7e09cf3f6789c06de45d84ef67eca86376ac
a <dsnake@protonmail.com>
2025-06-23 03:29:23 -0400

    use original defaults, clean up code/builders

--- orbotservice/src/main/java/org/torproject/android/service/TProxyService.java
+++ orbotservice/src/main/java/org/torproject/android/service/TProxyService.java
@@ -9,7 +9,6 @@ public class TProxyService {
         System.loadLibrary("hev-socks5-tunnel");
     }
 
-    public static final String IPV4 = "192.168.0.1";
     public static final int TASK_SIZE = 81920;
     public static final int TUNNEL_MTU = 8500;
 }
